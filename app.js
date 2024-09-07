befee2e6b70b161e399322907e8d0f7daba5cdc0
Rakibul2580 <rh565153@gmail.com>
2024-09-07 23:11:35 +0300

    Hello

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ async function run() {
     const Teachers = database.collection("Teachers");
 
     app.get("/", async (req, res) => {
-      res.send("hello world.");
+      res.send("hello world");
     });
 
     //For Front End
