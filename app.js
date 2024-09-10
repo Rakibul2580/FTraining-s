3f17791e310b77c3bd201f5f932003b3d207e793
moriyam-mohona <moriyammohona@gmail.com>
2024-09-10 15:18:01 +0600

    authentication and save user data

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = 5000;
 
@@ -18,11 +19,56 @@ async function run() {
   try {
     await client.connect();
     const database = client.db("Orvido-School");
+    const Users = database.collection("Users");
     const Students = database.collection("Students");
     const Teachers = database.collection("Teachers");
 
     app.get("/", async (req, res) => {
       res.send("hello world.");
+    });
+
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "24h",
+      });
+      res.send({ token });
+    });
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(403).send({ message: "No token provided" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Invalid token" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    // For New Users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await Users.findOne(query);
+      if (existingUser) {
+        return res.send({
+          message: "user already exists",
+          insertedId: null,
+        });
+      }
+      const result = await Users.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", verifyToken, async (req, res) => {
+      const result = await Users.find().toArray();
+      res.send(result);
     });
 
     //For Front End
