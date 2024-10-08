86895020bc507e6e7cf16aceb45c4701ad6e577a
moriyam-mohona <moriyammohona@gmail.com>
2024-10-08 15:27:59 +0600

    server comment delete

--- index.js
+++ index.js
@@ -84,7 +84,6 @@ async function run() {
         res.send(result);
       }
     });
-    // ---------------
     // Fetch user by email [Delete]
     // app.get("/users/:email", async (req, res) => {
     //   const email = req.params.email;
